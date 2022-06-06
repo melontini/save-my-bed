31e1ecde2d83298bbdf08c770d3570036e08c51b
melontini <melontini@yandex.ru>
2022-06-06 18:11:33 +0700

    Initial commit

---
+++ src/main/java/me/melontini/savemybed/SaveMyBed.java
@@ -0,0 +1,9 @@
+package me.melontini.savemybed;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SaveMyBed implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
