e6fa78d77405a8aa8633ec936219473916c4cacf
melontini <melontini@yandex.ru>
2022-06-06 18:32:50 +0700

    gradlew stuff

--- src/main/java/me/melontini/savemybed/mixin/BedBlockMixin.java
+++ src/main/java/me/melontini/savemybed/mixin/BedBlockMixin.java
@@ -14,13 +14,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import static net.minecraft.block.BedBlock.isOverworld;
+import static net.minecraft.block.BedBlock.isBedWorking;
 
 @Mixin(BedBlock.class)
 public class BedBlockMixin {
     @Inject(at = @At("HEAD"), method = "onUse", cancellable = true)
     public void onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
-        if (!isOverworld(world)) {
+        if (!isBedWorking(world)) {
             player.sendMessage(new TranslatableText("savemybed.action"), true);
             cir.setReturnValue(ActionResult.SUCCESS);
             cir.cancel();
