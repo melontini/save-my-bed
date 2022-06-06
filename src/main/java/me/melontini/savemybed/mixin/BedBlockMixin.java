cff633dc6499045bd825f2a493fc5ddf93b0b30d
melontini <melontini@yandex.ru>
2022-06-06 19:17:18 +0700

    translation fix

--- src/main/java/me/melontini/savemybed/mixin/BedBlockMixin.java
+++ src/main/java/me/melontini/savemybed/mixin/BedBlockMixin.java
@@ -27,7 +27,7 @@ public abstract class BedBlockMixin extends HorizontalFacingBlock implements Blo
     @Inject(at = @At("HEAD"), method = "onUse", cancellable = true)
     public void onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
         if (!isBedWorking(world)) {
-            player.sendMessage(new TranslatableText("savemybed.action"), true);
+            player.sendMessage(new TranslatableText("text.savemybed.action"), true);
             cir.setReturnValue(ActionResult.SUCCESS);
             cir.cancel();
         }
